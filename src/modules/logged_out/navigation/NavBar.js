1a21b20ca246ca75d572159d94fcb4ed1ceb2330
sagun03 <sagunsaluja13@gmail.com>
2020-07-20 19:03:09 +0530

    WIP

--- src/modules/logged_out/navigation/NavBar.js
+++ src/modules/logged_out/navigation/NavBar.js
@@ -6,6 +6,8 @@ import {
   Toolbar,
   Typography,
   IconButton,
+  Button,
+  Hidden,
   withStyles
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
@@ -20,6 +22,10 @@ const styles = theme => ({
     boxShadow: theme.shadows[6],
     backgroundColor: theme.palette.common.white
   },
+  appBar1: {
+    boxShadow: theme.shadows[6],
+    backgroundColor: '#3864cc',
+  },
   toolbar: {
     display: "flex",
     justifyContent: "space-between"
@@ -27,13 +33,21 @@ const styles = theme => ({
   menuButton: {
     marginRight: theme.spacing(2),
   },
+  menuButtonText1 : {
+    color: 'white',
+  },
   brandText: {
     fontFamily: "'Baloo Bhaijaan', cursive",
     fontWeight: 500
   },
+  brandText1: {
+    fontFamily: "'Blippo', fantasy",
+    fontWeight: 'bold'
+  },
   noDecoration: {
     textDecoration: "none !important"
   }
+  
 });
 
 const NavBar = (props) => {
@@ -44,17 +58,18 @@ const NavBar = (props) => {
     handleDrawerOpen,
     handleDrawerClose,
     isDrawerOpen,
-    selectedTab
+    selectedTab,
+    style,
   } = props;
   const menuItems = [
     {
-      link: "/home",
-      name: "Home",
+      link: "/selected-dashboard",
+      name: "dashboard",
       icon: <HomeIcon className="text-white" />
     },
     {
-      link: "/board",
-      name: "Board",
+      link: "/board-list",
+      name: "Boards",
       icon: <BookIcon className="text-white" />
     },
     {
@@ -70,34 +85,69 @@ const NavBar = (props) => {
   ];
   return (
     <div className={classes.root}>
-      <AppBar position="static" className={classes.appBar}>
+      <AppBar position="static" className={style ? classes.appBar1 : classes.appBar}>
         <Toolbar className={classes.toolbar}>
-            <IconButton
-                className={classes.menuButton}
-                onClick={handleDrawerOpen}
-                aria-label="menu"
-                color="inherit"
-                edge="start"
-              >
-                <MenuIcon color="primary" />
-              </IconButton>
           <div>
             <Typography
               variant="h4"
-              className={classes.brandText}
+              className={style ? classes.brandText1 : classes.brandText}
               display="inline"
-              color="primary"
+              color={style ? 'white' : 'primary'}
+              classes={style ? { root: classes.menuButtonText1 } : ''}
             >
               Re
             </Typography>
             <Typography
               variant="h4"
-              className={classes.brandText}
+              className={style ? classes.brandText1 : classes.brandText}
               display="inline"
-              color="secondary"
+              color={style ? 'white' : 'secondary'}
+              classes={style ? { root: classes.menuButtonText1 } : ''}
             >
               Tro
             </Typography>
+          </div>
+          <div>
+            <Hidden mdUp>
+              <IconButton
+                className={classes.menuButton}
+                onClick={handleDrawerOpen}
+                aria-label="Open Navigation"
+              >
+                <MenuIcon color="primary" />
+              </IconButton>
+            </Hidden>
+            <Hidden smDown>
+              {menuItems.map(element => {
+                if (element.link) {
+                  return (
+                    <Link
+                      key={element.name}
+                      to={element.link}
+                      className={classes.noDecoration}
+                      onClick={handleDrawerClose}
+                    >
+                      <Button
+                        size="large"
+                        classes={style ? { root: classes.menuButtonText1 } : { text: classes.menuButtonText }}
+                        >
+                        {element.name}
+                      </Button>
+                    </Link>
+                  );
+                }
+                return (
+                  <Button
+                  size="large"
+                    onClick={element.onClick}
+                    classes={style ? { root: classes.menuButtonText1 } : { text: classes.menuButtonText }}
+                    key={element.name}
+                  >
+                    {element.name}
+                  </Button>
+                );
+              })}
+            </Hidden>
           </div>
         </Toolbar>
       </AppBar>
--- src/theme.js
+++ src/theme.js
@@ -9,7 +9,7 @@ const background = "#f5f5f5";
 const warningLight = "rgba(253, 200, 69, .3)";
 const warningMain = "rgba(253, 200, 69, .5)";
 const warningDark = "rgba(253, 200, 69, .7)";
-
+const white = "white";
 // border
 const borderWidth = 2;
 const borderColor = "rgba(0, 0, 0, 0.13)";
@@ -26,7 +26,7 @@ const spacing = 8;
 
 const theme = createMuiTheme({
   palette: {
-    primary: { main: primary },
+    primary: { main: primary, white: white },
     secondary: { main: secondary },
     common: {
       black,
